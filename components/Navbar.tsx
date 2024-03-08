4f2aa8b2be86ef322de8d6a9a0a6cdd819ec7f1a
Aditya <codecommander03@gmail.com>
2024-03-08 19:46:19 +0530

    default

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const navIcons = [
+    { src: '/assets/icons/search.svg', alt: 'search' },
+    { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+    { src: '/assets/icons/user.svg', alt: 'user' },
+]
+
+const Navbar = () => {
+    return (
+        <header className='w-full'>
+            <nav className='nav'>
+                <Link href="/" className='flex items-center gap-1'>
+                    <Image
+                        src="/assets/icons/logo.svg"
+                        width={27}
+                        height={27}
+                        alt="logo"
+                    />
+                    <p className='nav-logo'>
+                        Scout <span className='text-primary'>Shop</span>
+                    </p>
+                </Link>
+
+                <div className='flex items-center gap-5'>
+                    {navIcons.map((icon) => (
+                        <Image
+                            src={icon.src}
+                            width={27}
+                            height={27}
+                            alt={icon.alt}
+                            className='object-contain'
+                        />
+                    ))}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
