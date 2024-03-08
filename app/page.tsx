6f1062d2832f73847205dade8f3ce1d1a839221f
Aditya <codecommander03@gmail.com>
2024-03-08 23:23:20 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image"
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping starts here:
               <Image
-                src='/assets/icons/arrow-right.svg'
+                src="/assets/icons/arrow-right.svg"
                 width={16}
                 height={16}
                 alt="arrow-right"
@@ -17,13 +18,32 @@ const Home = () => {
             </p>
 
             <h1 className="head-text">
-
+              Unleash the power of{" "}
+              <span className="text-primary">ScoutShop</span>
             </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+
+            <Searchbar/>
           </div>
+          HeroCarousel
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gay-y-16">
+          {['Phone','Book','Shoes'].map((product) => (
+          <div>{product}</div>
+          ))}
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default Home
